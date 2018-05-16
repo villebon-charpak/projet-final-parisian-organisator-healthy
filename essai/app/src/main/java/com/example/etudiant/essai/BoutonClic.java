c6dedbf4f3a5dc86ad69fdf09656b5b4241acdb1
MaelleBgn <maelle.brugnon@u-psud.fr>
2018-05-16 15:30:22 +0200

    Finition du bouton clic

--- essai/app/src/main/java/com/example/etudiant/essai/BoutonClic.java
+++ essai/app/src/main/java/com/example/etudiant/essai/BoutonClic.java
@@ -2,12 +2,32 @@ package com.example.etudiant.essai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BoutonClic extends AppCompatActivity {
+
+    private TextView points;
+    private ImageView bouton;
+    private clicks = 0;     /** permet de mettre le compteur à 0 */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bouton_clic);
+
+        this.points = (TextView) findViewById(R.id.points);
+        this.bouton = (ImageView) findViewById(R.id.bouton);
+
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+            clicks++;
+            points.setText("points : " + clicks ); /** permet de relier l'image et le compteur. Le compteur va afficher le bon nombre de point */
+
+            }
+        });
     }
 }
